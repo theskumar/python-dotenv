ba8c629af76161254fd4fe047b2a74f3094a58a4
Saurabh Kumar <me+github@saurabh-kumar.com>
2014-09-08 14:00:34 +0530

    add bumpversion and update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python-dotenv",
-    description=".env file settings for non-heroku setups",
+    description="Add .env support to your django/flask apps in development and deployments",
     version="0.1.0",
     author="Saurabh Kumar",
     author_email="me+github@saurabh-kumar.com",
@@ -16,10 +16,39 @@ setup(
         dotenv=dotenv:cli
     ''',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.3',
+        # 'Programming Language :: Python :: 2.4',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        # 'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+        'Environment :: Web Environment',
+        # 'Framework :: Django',
     ]
 )
+
+# (*) Please direct queries to the discussion group, rather than to me directly
+#     Doing so helps ensure your question is helpful to other users.
+#     Queries directly to my email are likely to receive a canned response.
+#
+#     Many thanks for your understanding.
