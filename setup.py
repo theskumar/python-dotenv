a139e7eb83d68d16f4fac64292d9ef36a93ab889
José Padilla <jpadilla@webapplicate.com>
2014-12-06 10:00:26 -0400

    Cleanup and update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
 from setuptools import setup
 
+
+def get_version(module):
+    """
+    Return package version as listed in `__version__`.
+    """
+    init_py = open('{0}.py'.format(module)).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('dotenv')
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print('You probably want to also tag the version now:')
+    print('  git tag -a {0} -m "version {0}"'.format(version, version))
+    print('  git push --tags')
+    sys.exit()
+
+
 setup(
-    name = "django-dotenv",
-    description = "foreman reads from .env. manage.py doesn't. Let's fix that.",
-    version = "1.2",
-    author = "Jacob Kaplan-Moss",
-    author_email = "jacob@jacobian.org",
-    url = "http://github.com/jacobian/django-dotenv",
-    py_modules = ['dotenv'],
-    test_suite = 'tests',
-    classifiers = [
-        'Development Status :: 4 - Beta',
+    name='django-dotenv',
+    description="foreman reads from .env. manage.py doesn't. Let's fix that.",
+    version=version,
+    author='Jacob Kaplan-Moss',
+    author_email='jacob@jacobian.org',
+    maintainer='José Padilla',
+    maintainer_email='hello@jpadilla.com',
+    url='http://github.com/jpadilla/django-dotenv',
+    py_modules=['dotenv'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
     ]
 )
