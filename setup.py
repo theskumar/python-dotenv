f7f049a71e27af53ba40e861d688910bdf9aaac0
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-02-28 21:59:46 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Programming Language :: Python',
@@ -44,7 +44,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         # 'Topic :: System :: Systems Administration',
-        'Topic :: Utilities'
+        'Topic :: Utilities',
         'Environment :: Web Environment',
         # 'Framework :: Django',
     ]
