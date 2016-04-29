b9ee6f5208a22a6d4aa73848d875645752b4e120
Isaac Slavitt <isaac.slavitt@gmail.com>
2016-04-29 18:27:32 -0400

    Add method to walk up directories looking for .env
    - Add `find_dotenv` method that will try to find a .env file by
      (a) guessing where to start using `__file__` or the working
      directory -- allowing this to work in non-file contexts such as
      IPython notebooks and the REPL, and then (b) walking up the dir-
      ectory tree looking for the specified file -- called `.env` by
      default. This is a bit like the "filthy magic" employed by
      django-dotenv[1] to serve the same purpose, and allows the user
      to write `load_dotenv(find_dotenv())` in many contexts.
    - Add test for new function
    
    [1] https://github.com/jpadilla/django-dotenv/blob/master/dotenv.py#L44-L46

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,10 @@
+# -*- coding: utf8 -*-
+import os
+import pytest
+import tempfile
 import warnings
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
 def test_warns_if_file_does_not_exist():
@@ -10,3 +14,44 @@ def test_warns_if_file_does_not_exist():
         assert len(w) == 1
         assert w[0].category is UserWarning
         assert str(w[0].message) == "Not loading .does_not_exist - it doesn't exist."
+
+
+def test_find_dotenv():
+    """
+    Create a temporary folder structure like the following:
+
+        tmpXiWxa5/
+        └── child1
+            ├── child2
+            │   └── child3
+            │       └── child4
+            └── .env
+
+    Then try to automatically `find_dotenv` starting in `child4`
+    """
+    tmpdir = tempfile.mkdtemp()
+
+    curr_dir = tmpdir
+    dirs = []
+    for f in ['child1', 'child2', 'child3', 'child4']:
+        curr_dir = os.path.join(curr_dir, f)
+        dirs.append(curr_dir)
+        os.mkdir(curr_dir)
+
+    child1, child4 = dirs[0], dirs[-1]
+
+    # change the working directory for testing
+    os.chdir(child4)
+
+    # try without a .env file and force error
+    with pytest.raises(IOError):
+        find_dotenv(raise_error_if_not_found=True, usecwd=True)
+
+    # try without a .env file and fail silently
+    assert find_dotenv(usecwd=True) == ''
+
+    # now place a .env file a few levels up and make sure it's found
+    filename = os.path.join(child1, '.env')
+    with open(filename, 'w') as f:
+        f.write("TEST=test\n")
+    assert find_dotenv(usecwd=True) == filename
