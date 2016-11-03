bdf373ff6b0add8f5536435fd2ac56ad7293a389
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-11-03 14:30:40 +0000

    Add ipython magic.

---
+++ dotenv/ipython.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import os
+from .main import load_dotenv, find_dotenv
+
+
+def _magic(dotenv_path):
+    """
+    dotenv [dotenv_path]
+    
+    Search in increasingly higher folders for the `dotenv_path`
+    """
+    # Locate the .env file
+    dotenv_path = dotenv_path or '.env'
+    try:
+        dotenv_path = find_dotenv(dotenv_path, True)
+    except IOError:
+        print("cannot find .env file")
+        return
+    
+    # Load the .env file
+    load_dotenv(dotenv_path)
+
+
+def load_ipython_extension(ipython):
+    """Register the %dotenv magic."""
+    ipython.register_magic_function(_magic, magic_name='dotenv')
