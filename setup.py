54a13771f4972f1f859d70985c10e30b7c9a1600
hanan.shteingart <hanan.shteingart@gong.io>
2017-01-31 11:46:46 +0200

    fix utf-8 encoding when reading the readme.rst

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding="utf8") as readme_file:
     readme = readme_file.read()
 
 setup(
