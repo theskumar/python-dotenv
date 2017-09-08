aa57fdef5051835c3e27c6cca5f4e35ebb82d4f7
Saurabh Kumar <saurabh@saurabh-kumar.com>
2017-09-08 15:55:29 +0530

    Remove hard dependency on iPython

--- dotenv/__init__.py
+++ dotenv/__init__.py
@@ -1,5 +1,8 @@
 from .cli import get_cli_string
 from .main import load_dotenv, get_key, set_key, unset_key, find_dotenv
-from .ipython import load_ipython_extension
+try:
+    from .ipython import load_ipython_extension
+except ImportError:
+    pass
 
 __all__ = ['get_cli_string', 'load_dotenv', 'get_key', 'set_key', 'unset_key', 'find_dotenv', 'load_ipython_extension']
