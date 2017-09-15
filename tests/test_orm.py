1bf7b7ff73617f578d69eba7e7c70121b4f188a2
winkidney <winkidney@gmail.com>
2017-09-15 12:50:45 +0000

    Make finalizer "del os.environ" compatible with different style

--- tests/test_orm.py
+++ tests/test_orm.py
@@ -19,6 +19,8 @@ def set_base_env(request):
 
     def finalizer():
         for key, value in values:
+            if key in os.environ:
+                del os.environ[key]
             os.unsetenv(key)
 
     request.addfinalizer(finalizer=finalizer)
