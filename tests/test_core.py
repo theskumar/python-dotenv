250219295d3620a69881d5befab6f34d36d272e7
Alan Justino <alan.justino@yahoo.com.br>
2018-01-15 14:10:11 -0200

    Test: parse_dotenv accepting a filelike stream

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,6 +4,7 @@ import pytest
 import tempfile
 import warnings
 import sh
+from io import BytesIO
 
 from dotenv import load_dotenv, find_dotenv, set_key
 from IPython.terminal.embed import InteractiveShellEmbed
@@ -110,3 +111,12 @@ def test_ipython_override():
     ipshell.magic("load_ext dotenv")
     ipshell.magic("dotenv -o")
     assert os.environ["MYNEWVALUE"] == 'q1w2e3'
+
+
+def test_parse_dotenv_stream():
+    stream = BytesIO('DOTENV=WORKS\n')
+    stream.seek(0)
+    parsed_generator = parse_dotenv(stream=stream)
+    parsed_dict = dict(iter(parsed_generator))
+    assert 'DOTENV' in parsed_dict
+    assert parsed_dict['DOTENV'] == 'WORKS'
