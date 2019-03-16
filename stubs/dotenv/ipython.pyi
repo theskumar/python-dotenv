2981c42e2660d320862e252849904f0d13d35225
Masaki Hara <ackie.h.gmai@gmail.com>
2019-03-16 19:36:37 +0900

    Remove ./stubs

--- stubs/dotenv/ipython.pyi
+++
@@ -1,7 +0,0 @@
-from IPython.core.magic import Magics  # type: ignore
-from typing import Any
-
-class IPythonDotEnv(Magics):
-    def dotenv(self, line: Any) -> None: ...
-
-def load_ipython_extension(ipython: Any) -> None: ...
