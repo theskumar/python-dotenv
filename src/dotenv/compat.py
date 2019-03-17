770d7c96de2d97aea078dae77678a29e46015f95
Masaki Hara <ackie.h.gmai@gmail.com>
2019-03-17 11:19:30 +0900

    Text can be reused for text_type.

--- src/dotenv/compat.py
+++ src/dotenv/compat.py
@@ -1,4 +1,4 @@
-from typing import Text, Type, TYPE_CHECKING
+from typing import Text, Type
 import sys
 if sys.version_info >= (3, 0):
     from io import StringIO  # noqa
@@ -7,7 +7,4 @@ else:
 
 PY2 = sys.version_info[0] == 2  # type: bool
 WIN = sys.platform.startswith('win')  # type: bool
-if TYPE_CHECKING:
-    text_type = Text  # type: Type[Text]
-else:
-    text_type = unicode if PY2 else str  # noqa
+text_type = Text  # type: Type[Text]
