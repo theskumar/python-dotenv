096524d45e6706c05b0e7a9910f862d5eae24876
Saurabh Kumar <theskumar@users.noreply.github.com>
2021-10-09 13:26:42 +0530

    Add Python 3.10 support

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
