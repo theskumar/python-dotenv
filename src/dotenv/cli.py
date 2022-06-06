d68fad5a18d7880f1dc29d709c68a9524393b714
ProfessorPiggos <doawk7@gmail.com>
2022-06-06 16:34:17 -0500

    added more cleanup to make behavior more consistent
    equals sign cleanup added

--- src/dotenv/cli.py
+++ src/dotenv/cli.py
@@ -138,7 +138,7 @@ def generate_sample(ctx: click.Context) -> None:
     newEnvList = []
     with open(filedir,'r') as file:
         for line in file:
-            line = " ".join(line.strip().split())
+            line = line.replace(" ", "").replace("#", "# ").replace("=", "= ")
             if line[0] != "#":
                 line = line.split("=", 1)[0] + "="
             newEnvList.append(line)
