eacbe1537d1aa0f0d0d4fca580ea3ef92fd6d168
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-02-23 12:48:21 +0530

    Add python 3.12 alpha

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
