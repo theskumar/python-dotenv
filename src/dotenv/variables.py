a8e26bee6bcea2596e5a5573de7cc3bd601f2a74
btee <b.kramarski@gmail.com>
2023-11-13 22:30:59 +0100

    Support default, required and alternative expansions
    Default expansion is more strict now (undefined vs. empty).
    The supported set is closer to what is available in docker-compose
    See: https://docs.docker.com/compose/environment-variables/env-file/

--- src/dotenv/variables.py
+++ src/dotenv/variables.py
@@ -5,9 +5,9 @@ from typing import Iterator, Mapping, Optional, Pattern
 _posix_variable: Pattern[str] = re.compile(
     r"""
     \$\{
-        (?P<name>[^\}:]*)
-        (?::-
-            (?P<default>[^\}]*)
+        (?P<name>[^\}:+?-]*)
+        (?:
+            (?P<action_spec>:?[+?-])(?P<argument>[^\}]*)
         )?
     \}
     """,
@@ -45,25 +45,60 @@ class Literal(Atom):
         return self.value
 
 
-class Variable(Atom):
-    def __init__(self, name: str, default: Optional[str]) -> None:
-        self.name = name
-        self.default = default
+class Action:
+    def __init__(self, spec: str, argument: str = ""):
+        assert len(spec) > 0
+        self.spec = spec
+        self.argument = argument
 
     def __repr__(self) -> str:
-        return f"Variable(name={self.name}, default={self.default})"
+        return f"Action(spec={self.spec}, argument={self.argument})"
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, self.__class__):
             return NotImplemented
-        return (self.name, self.default) == (other.name, other.default)
+        return (self.spec, self.argument) == (other.spec, other.argument)
 
     def __hash__(self) -> int:
-        return hash((self.__class__, self.name, self.default))
+        return hash((self.__class__, self.spec, self.argument))
+
+    def resolves_empty(self, value: Optional[str]):
+        if value is None:
+            return True
+        return self.spec[0] == ":" and value == ""
+
+    def resolve(self, name: str, value: Optional[str]):
+        empty = self.resolves_empty(value)
+        action = self.spec[-1]
+        if action == "-" and empty:
+            return self.argument
+        if action == "+" and not empty:
+            return self.argument
+        if action == "?" and empty:
+            raise LookupError(f"{name}: {self.argument}")
+        return value
+
+
+class Variable(Atom):
+    def __init__(self, name: str, action: Optional[Action]) -> None:
+        self.name = name
+        self.action = action
+
+    def __repr__(self) -> str:
+        return f"Variable(name={self.name}, action={repr(self.action)})"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return (self.name, self.action) == (other.name, other.action)
+
+    def __hash__(self) -> int:
+        return hash((self.__class__, self.name, self.action))
 
     def resolve(self, env: Mapping[str, Optional[str]]) -> str:
-        default = self.default if self.default is not None else ""
-        result = env.get(self.name, default)
+        result = env.get(self.name, None)
+        if self.action is not None:
+            result = self.action.resolve(self.name, result)
         return result if result is not None else ""
 
 
@@ -73,12 +108,14 @@ def parse_variables(value: str) -> Iterator[Atom]:
     for match in _posix_variable.finditer(value):
         (start, end) = match.span()
         name = match["name"]
-        default = match["default"]
+        action_spec = match["action_spec"]
+        argument = match["argument"]
+        action = Action(action_spec, argument) if action_spec else None
 
         if start > cursor:
             yield Literal(value=value[cursor:start])
 
-        yield Variable(name=name, default=default)
+        yield Variable(name=name, action=action)
         cursor = end
 
     length = len(value)
