d61c1edfbce2f80cd498b0e7152fb71854554d5c
duarte-pompeu <duarte-pompeu@users.noreply.github.com>
2023-12-08 12:25:37 +0000

    remove unecessary param

--- src/dotenv/main.py
+++ src/dotenv/main.py
@@ -117,14 +117,13 @@ def get_key(
     dotenv_path: StrPath,
     key_to_get: str,
     encoding: Optional[str] = "utf-8",
-    verbose: bool = True,
 ) -> Optional[str]:
     """
     Get the value of a given key from the given .env.
 
     Returns `None` if the key isn't found or doesn't have a value.
     """
-    return DotEnv(dotenv_path, verbose=verbose, encoding=encoding).get(key_to_get)
+    return DotEnv(dotenv_path, verbose=True, encoding=encoding).get(key_to_get)
 
 
 @contextmanager
