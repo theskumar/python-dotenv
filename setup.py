fcc923eb7c3a958304955724efee41272481320f
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-10-07 22:34:57 -0600

    Add Python 3.13 trove classifier

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
