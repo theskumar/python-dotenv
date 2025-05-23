9676b1bc64af8aaa02ca2d1e6012df8c960b44d6
Balaje Suri <balajesuri@gmail.com>
2025-05-23 11:02:55 +0200

    skip 000 permission tests for root user

--- tests/test_main.py
+++ tests/test_main.py
@@ -61,6 +61,7 @@ def test_set_key_encoding(dotenv_path):
     assert dotenv_path.read_text(encoding=encoding) == "a='é'\n"
 
 
+@pytest.mark.skipif(os.geteuid() == 0, reason="Root user can access files even with 000 permissions.")
 def test_set_key_permission_error(dotenv_path):
     dotenv_path.chmod(0o000)
 
@@ -167,6 +168,7 @@ def test_unset_encoding(dotenv_path):
     assert dotenv_path.read_text(encoding=encoding) == ""
 
 
+@pytest.mark.skipif(os.geteuid() == 0, reason="Root user can access files even with 000 permissions.")
 def test_set_key_unauthorized_file(dotenv_path):
     dotenv_path.chmod(0o000)
 
